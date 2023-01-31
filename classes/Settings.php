7db10a1d8f81ba93f643557c7a05b584c9253070
Josh Pollock <jpollock412@gmail.com>
2023-01-31 17:54:53 -0500

    Basic settings class

--- classes/Settings.php
+++ classes/Settings.php
@@ -1,8 +1,51 @@
 <?php
+
 namespace VendorNamespace\PluginNamespace;
 
-class Settings {
+class Settings
+{
 
+    /**
+     * Name of the option settings are saved in
+     *
+     * @since 0.0.1
+     *
+     * @var string
+     */
+    protected $optionName = 'pm2-modern-plugin-settings';
+
+
+    /**
+     * Defaults
+     *
+     * @since 0.0.1
+     *
+     * @param array $defaults
+     */
+    protected $defaults = [
+        'apiKey' => '',
+    ];
+
+    /**
+     * Save settings
+     *
+     * @since 0.0.1
+     *
+     * @param array $data
+     * @return void
+     */
+    public function save(array $data)
+    {
+        $data = array_merge($this->defaults, $data);
+        /**
+         * Filter settings before saving
+         *
+         * @since 0.0.1
+         * @param array $data Data to save
+         */
+        $data = apply_filters('ACTION_PREFIX_save_settings', $data);
+        update_option($this->optionName,);
+    }
     /**
      * Get all settings
      *
@@ -10,9 +53,15 @@ class Settings {
      *
      * @return array
      */
-    public function getAll(){
-        return [
-            'foo' => 'bar'
-        ];
+    public function getAll()
+    {
+        $values =  get_option($this->optionName, $this->defaults);
+        /**
+         * Filter settings before returning
+         *
+         * @since 0.0.1
+         * @param array $values Settings
+         */
+        return apply_filters('ACTION_PREFIX_get_settings', $values);
     }
 }
