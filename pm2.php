54bff9c6567a3559aad95dc3f42a506f5cbb7b91
Josh Pollock <jpollock412@gmail.com>
2023-01-31 16:49:32 -0500

    composer.json

--- pm2.php
+++ pm2.php
@@ -2,8 +2,20 @@
 
 $replacments = [
     'slug' => 'pm2-modern-plugin',
+    'composer' => [
+        'vendor' => 'vendor-slug',
+        'package' => 'plugin',
+        'VendorNamespace' => 'VendorNamespace',
+        'PluginNamespace' => 'PluginNamespace',
+    ],
     'description' => 'PLUGIN_DESCRIPTION',
     'author' => [
         'name' => 'PLUGIN_AUTHOR_NAME',
+    ],
+    'github' => [
+        'repo' => 'PLUGIN_GITHUB_REPO',
+    ],
+    'plugin' => [
+        'uri' => 'PLUGIN_URI',
     ]
 ];
